d69b0bce46ce5b9f25faef88a64d5a7a183f1c59
WaltMorrow <wmorrow283@noscihigh.org>
2021-03-22 19:53:24 +0000

    coding

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ num1 & num2.
 
 add should return the sum of these two parameters
 */
-
-
+var num1 = 9
+var num2 = 10
+function add(num1, num2){
+  return num1 + num2
+}
 
 
 
@@ -21,7 +24,7 @@ Use a function(PROMPT TWO)
 Declare a function called printer
 The printer function should print to the console no return or parameter needed
 */
-
+print.to.console(function printer(){})
 
 
 
@@ -54,3 +57,9 @@ This function will return the paramater
 Call the addKeys function outside its function definition and 
 pass myObject as an argument.
 */
+var myObject = chair
+function addKeys(object){
+  var firstName = "Walter"
+  var lastName = "Morrow"
+  return firstName + ' ' + lastName
+}
